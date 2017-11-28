f40f0cad9b44a99e6756442e08e8eaa60f4f055b
Nazhath2124 <nazhathaara.md@gmail.com>
2017-11-28 11:58:04 +0530

    version controlled maventest

---
+++ src/main/java/com/Nazhath/app/App.java
@@ -0,0 +1,13 @@
+package com.Nazhath.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
